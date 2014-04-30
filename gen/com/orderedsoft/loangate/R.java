3895f28127877a154cdaebed92336373f3dc3d0b
Hamed <HamedMosavi@Yahoo.com>
2014-04-30 17:04:26 +0430

    R Changed app icon.  * Fixed Gui "Under Construction" text.

--- gen/com/orderedsoft/loangate/R.java
+++ gen/com/orderedsoft/loangate/R.java
@@ -48,22 +48,21 @@ public final class R {
         public static final int education=0x7f020005;
         public static final int graduation=0x7f020006;
         public static final int home=0x7f020007;
-        public static final int ic_launcher=0x7f020008;
-        public static final int icon1=0x7f020009;
-        public static final int list=0x7f02000a;
-        public static final int list_item_background=0x7f02000b;
-        public static final int manufactory=0x7f02000c;
-        public static final int plane=0x7f02000d;
-        public static final int repair=0x7f02000e;
-        public static final int ring=0x7f02000f;
-        public static final int search=0x7f020010;
-        public static final int settings=0x7f020011;
-        public static final int store=0x7f020012;
-        public static final int tab_header_background=0x7f020013;
-        public static final int tab_header_divider=0x7f020014;
-        public static final int tab_header_foreground=0x7f020015;
-        public static final int tab_header_led=0x7f020016;
-        public static final int wedding=0x7f020017;
+        public static final int list=0x7f020008;
+        public static final int list_item_background=0x7f020009;
+        public static final int manufactory=0x7f02000a;
+        public static final int money=0x7f02000b;
+        public static final int plane=0x7f02000c;
+        public static final int repair=0x7f02000d;
+        public static final int ring=0x7f02000e;
+        public static final int search=0x7f02000f;
+        public static final int settings=0x7f020010;
+        public static final int store=0x7f020011;
+        public static final int tab_header_background=0x7f020012;
+        public static final int tab_header_divider=0x7f020013;
+        public static final int tab_header_foreground=0x7f020014;
+        public static final int tab_header_led=0x7f020015;
+        public static final int wedding=0x7f020016;
     }
     public static final class id {
         public static final int action_settings=0x7f090016;
